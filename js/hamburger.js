c3b50379e321948a1bd1e43f9a044add3ef8de7f
mikaelrev <mikael.myrvang@gmail.com>
2021-12-15 18:29:30 +0100

    fixed styling on menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,13 +2,13 @@ const nav = document.querySelector('.navigation');
 const navToggle = document.querySelector('.nav-toggle');
 
 navToggle.addEventListener('click', () => {
-    const visibility = links.getAttribute('data-visible');
+    const visibility = nav.getAttribute('data-visible');
 
     if (visibility === "false") {
-        links.setAttribute('data-visible', true);
+        nav.setAttribute('data-visible', true);
         navToggle.setAttribute('aria-expanded', true);
     } else if (visibility === "true") {
-        links.setAttribute('data-visible', false);
+        nav.setAttribute('data-visible', false);
         navToggle.setAttribute('aria-expanded', false);
     }
 });
